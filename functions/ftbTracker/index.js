68ec852e6003ab305451a0cf42f235234e13ac27
Amir Sarrouj <asarrouj590@gmail.com>
2020-05-03 16:19:28 -0400

    got ftb working yay

--- functions/ftbTracker/index.js
+++ functions/ftbTracker/index.js
@@ -1,31 +1,42 @@
 const fs = require('fs');
+path = require('path');
 
-const filename = 'ftbDatabase.json';
+const filename = path.join(__dirname,'ftbDatabase.json');
 let ftbDatabase = JSON.parse(fs.readFileSync(filename))
 
 module.exports = (client) => {
-    client.on('message', msg => {
-        if (msg.includes('!ftb')){
-            msgParts = msg.split("/\s/");
+    client.on('message', (msg) => {
+        if (msg.content.includes('!ftb') && msg.author.username !== "Boofle"){
+            msgParts = msg.content.split(/\s/);
             if (msgParts.length === 1){
                 msg.channel.send(Object.entries(ftbDatabase).reduce((accMsg, ftbEntry) => {
-                    return accMsg += `'${ftbObj[0]}': ${ftbObj[1]}\n`;
+                    let user = msg.guild.members.cache.get(ftbEntry[0])/* || await client.fetchUser(ftbEntry[0])*/;
+                    return accMsg += `${user.displayName}: ${ftbEntry[1]}\n`;
                 }, "FTB STANDINGS:\n"))
             }
-            else if (msgParts.length === 3 && parseInt(msgParts[1]) !== NaN){
-                if (Object.keys(ftbDatabase).includes(msgParts[2])){
-                    ftbDatabase[msgParts[2]] += parseInt(msgParts[1]);
-                    msg.channel.send(`${msgParts[2]} now has ${ftbDatabase[msgParts[2]]} FTB points.`)
+            else if (msgParts.length === 3 && !Number.isNaN(parseInt(msgParts[1])) && /<@!\d+>/.test(msgParts[2])){
+                const id = /\d+/.exec(msgParts[2])[0];
+                let user = null;
+                try {
+                    console.log(id)
+                    user = msg.guild.members.cache.get(id)/* || await client.fetchUser(id)*/;
+                    console.log("done")
+                } catch (e){
+                    console.error(e)
+                }
+                if (Object.keys(ftbDatabase).includes(id)){
+                    ftbDatabase[id] += parseInt(msgParts[1]);
+                    msg.channel.send(`${user.displayName} now has ${ftbDatabase[id]} FTB points.`)
                 }
                 else{
-                    ftbDatabase[msgParts[2]] = parseInt(msgParts[1]);
-                    msg.channel.send(`User ${msgParts[2]} has now been created, starting with ${ftbDatabase[msgParts[2]]} FTB points.`);
+                    ftbDatabase[id] = parseInt(msgParts[1]);
+                    msg.channel.send(`User ${user.displayName} has now been created, starting with ${ftbDatabase[id]} FTB points.`);
                 }
             }
             else{
-                msg.channel.send("Sorry, that isn't a valid format for this command.\nThe correct format is \"!ftb (pointChange) (name)\", or simply \"!ftb\" to display current standings.")
+                msg.channel.send("Sorry, that isn't a valid format for this command.\nThe correct format is \"!ftb (pointChange) (@user)\", or simply \"!ftb\" to display current standings.")
             }
         }
-        fs.writeFileSync('filename', JSON.stringify(ftbDatabase));
+        fs.writeFileSync(filename, JSON.stringify(ftbDatabase));
     })
 }

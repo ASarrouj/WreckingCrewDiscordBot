9b4231bad520c7a5cc2a6c15e436e45f01dd61b4
Amir Sarrouj <asarrouj590@gmail.com>
2020-05-05 19:58:34 -0400

    abstracted the client on message call

--- functions/ftbTracker/index.js
+++ functions/ftbTracker/index.js
@@ -13,38 +13,36 @@ commands = [
 
 module.exports = {
     commands,
-    func: (client) => {
-        client.on('message', (msg) => {
-            if (msg.content.includes('!ftb') && msg.author.username !== "Boofle") {
-                msgParts = msg.content.trim().split(/\s+/);
-                if (msgParts.length === 1) {
-                    msg.channel.send(Object.entries(ftbDatabase).reduce((accMsg, ftbEntry) => {
-                        let user = msg.guild.members.cache.get(ftbEntry[0]);
-                        return accMsg += `${user.displayName}: ${ftbEntry[1]}\n`;
-                    }, "FTB STANDINGS:\n"))
+    func: (msg) => {
+        if (msg.content.includes('!ftb') && msg.author.username !== "Boofle") {
+            msgParts = msg.content.trim().split(/\s+/);
+            if (msgParts.length === 1) {
+                msg.channel.send(Object.entries(ftbDatabase).reduce((accMsg, ftbEntry) => {
+                    let user = msg.guild.members.cache.get(ftbEntry[0]);
+                    return accMsg += `${user.displayName}: ${ftbEntry[1]}\n`;
+                }, "FTB STANDINGS:\n"))
+            }
+            else if (msgParts.length === 3 && !Number.isNaN(parseInt(msgParts[1])) && /<@!?\d+>/.test(msgParts[2])) {
+                const id = /\d+/.exec(msgParts[2])[0];
+                let user = null;
+                try {
+                    user = msg.guild.members.cache.get(id);
+                } catch (e) {
+                    console.error(e)
                 }
-                else if (msgParts.length === 3 && !Number.isNaN(parseInt(msgParts[1])) && /<@!?\d+>/.test(msgParts[2])) {
-                    const id = /\d+/.exec(msgParts[2])[0];
-                    let user = null;
-                    try {
-                        user = msg.guild.members.cache.get(id);
-                    } catch (e) {
-                        console.error(e)
-                    }
-                    if (Object.keys(ftbDatabase).includes(id)) {
-                        ftbDatabase[id] += parseInt(msgParts[1]);
-                        msg.channel.send(`${user.displayName} now has ${ftbDatabase[id]} FTB points.`)
-                    }
-                    else {
-                        ftbDatabase[id] = parseInt(msgParts[1]);
-                        msg.channel.send(`User ${user.displayName} has now been created, starting with ${ftbDatabase[id]} FTB points.`);
-                    }
+                if (Object.keys(ftbDatabase).includes(id)) {
+                    ftbDatabase[id] += parseInt(msgParts[1]);
+                    msg.channel.send(`${user.displayName} now has ${ftbDatabase[id]} FTB points.`)
                 }
                 else {
-                    msg.channel.send("Sorry, that isn't a valid format for this command.\nThe correct format is \"!ftb (pointChange) (@user)\", or simply \"!ftb\" to display current standings.")
+                    ftbDatabase[id] = parseInt(msgParts[1]);
+                    msg.channel.send(`User ${user.displayName} has now been created, starting with ${ftbDatabase[id]} FTB points.`);
                 }
+                fs.writeFileSync(filename, JSON.stringify(ftbDatabase));
             }
-            fs.writeFileSync(filename, JSON.stringify(ftbDatabase));
-        })
+            else {
+                msg.channel.send("Sorry, that isn't a valid format for this command.\nThe correct format is \"!ftb (pointChange) (@user)\", or simply \"!ftb\" to display current standings.")
+            }
+        }
     }
 }
--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,16 @@
-const functions = [require('./chatting'),require('./ftbTracker')];
+const msgFunctions = [require('./chatting'),require('./ftbTracker')];
 module.exports = {
     init: (client) => {
-        functions.forEach((module) => {
-            module.func(client);
-        })
-        
-        const commands = functions.reduce((acc, module) => {
+                
+        const commands = msgFunctions.reduce((acc, module) => {
             return acc.concat(module.commands);
         }, [])
 
         client.on('message', msg => {
+            msgFunctions.forEach((module) => {
+                module.func(msg);
+            })
+
             if (msg.content === '!help') {
                 msg.channel.send(commands.reduce((accMsg, command) => {
                     return accMsg += `'${command.message}': ${command.info}\n`;

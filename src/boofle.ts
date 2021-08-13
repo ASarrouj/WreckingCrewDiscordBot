4220ca7d2a14ef08ea062ee2a24e5d45d87d0772
ASarrouj <asarrouj590@gmail.com>
2021-08-13 19:15:13 -0400

    boofle.ts intents

--- src/boofle.ts
+++ src/boofle.ts
@@ -2,13 +2,14 @@ import { Client, Intents } from 'discord.js';
 import { init } from './functions';
 import { loginToken } from './secureConstants.ign';
 
-const intents = [
-	Intents.FLAGS.GUILD_MEMBERS,
-	Intents.FLAGS.GUILD_MESSAGES,
-	Intents.FLAGS.DIRECT_MESSAGES,
-	Intents.FLAGS.GUILD_MESSAGE_REACTIONS
-];
-const client = new Client( { intents } );
+const client = new Client({
+	intents: [
+		Intents.FLAGS.GUILD_MEMBERS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.DIRECT_MESSAGES,
+		Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+	]
+});
 
 init(client);
 
